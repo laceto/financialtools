35085aa1fda7761a9ceba618917ed80438bcf7f5
laceto <l_aceto@hotmail.it>
2025-08-29 14:04:41 +0200

    add eval metrics and market comparison to parser

--- financialtools/pydantic_models.py
+++ financialtools/pydantic_models.py
@@ -25,6 +25,37 @@ from langchain_core.output_parsers import PydanticOutputParser
 #     )
 
 
+# class StockRegimeAssessment(BaseModel):
+#     ticker: str = Field(
+#         ..., description='The ticker of the stock under analysis'
+#     )
+#     regime: Literal["bull", "bear"] = Field(
+#         ..., description="The fundamental regime classification of the stock"
+#     )
+#     regime_rationale: str = Field(
+#         ..., description="Concise explanation justifying the regime classification based on the financial metrics, composite ratio and red flags"
+#     )
+#     metrics_movement: str = Field(
+#         ..., description=(
+#             "A summary description of how key financial metrics have moved across years, "
+#             "e.g., 'GrossMargin increased steadily, DebtToEquity rose sharply, FCFYield remained stable.'"
+#         )
+#     )
+#     non_aligned_findings: Optional[str] = Field(
+#         None,
+#         description=(
+#             "Observations or signals that are not aligned with the overall metric trends, "
+#             "such as contradictory indicators, anomalies."
+#         )
+#     )
+#     evaluation: Literal["overvaluated", "undervaluated", "fair"] = Field(
+#         ..., description="The valuation of the stock based on the Evaluation metrics"
+#     )
+#     evaluation_rationale: str = Field(
+#         ..., description="Concise explanation justifying the evaluation classification based on the financial metrics, composite ratio and red flags"
+#     )
+
+
 class StockRegimeAssessment(BaseModel):
     ticker: str = Field(
         ..., description='The ticker of the stock under analysis'
@@ -53,4 +84,9 @@ class StockRegimeAssessment(BaseModel):
     )
     evaluation_rationale: str = Field(
         ..., description="Concise explanation justifying the evaluation classification based on the financial metrics, composite ratio and red flags"
+    )
+    market_eval_metrics_comparison: str = Field(
+        ..., description=(
+            "A summary description of how stock metrics compare to the market metrics"
+        )
     )
