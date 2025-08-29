42ca8f6cc1d353694390daf42b8ba888f0b161ac
laceto <l_aceto@hotmail.it>
2025-08-29 14:14:26 +0200

    add weights by sector

--- financialtools/config.py
+++ financialtools/config.py
@@ -22,3 +22,127 @@ grouped_weights = {
         "FCFYield": 10
     }
 }
+
+
+sector_metric_weights = {
+    "Commercial Services": {
+        "GrossMargin": 10, "OperatingMargin": 12, "NetProfitMargin": 10,
+        "EBITDAMargin": 10, "ROA": 10, "ROE": 12,
+        "DebtToEquity": 12, "DebtToAssets": 10,
+        "CurrentRatio": 8, "FCFToRevenue": 8, "FCFYield": 8
+    },
+    "Communications": {
+        "GrossMargin": 10, "OperatingMargin": 12, "NetProfitMargin": 10,
+        "EBITDAMargin": 12, "ROA": 10, "ROE": 10,
+        "DebtToEquity": 10, "DebtToAssets": 8,
+        "CurrentRatio": 8, "FCFToRevenue": 10, "FCFYield": 10
+    },
+    "Consumer Durables": {
+        "GrossMargin": 10, "OperatingMargin": 12, "NetProfitMargin": 10,
+        "EBITDAMargin": 10, "ROA": 10, "ROE": 12,
+        "DebtToEquity": 12, "DebtToAssets": 10,
+        "CurrentRatio": 8, "FCFToRevenue": 8, "FCFYield": 8
+    },
+    "Consumer Non-Durables": {
+        "GrossMargin": 12, "OperatingMargin": 12, "NetProfitMargin": 10,
+        "EBITDAMargin": 10, "ROA": 10, "ROE": 10,
+        "DebtToEquity": 10, "DebtToAssets": 8,
+        "CurrentRatio": 8, "FCFToRevenue": 10, "FCFYield": 10
+    },
+    "Consumer Services": {
+        "GrossMargin": 10, "OperatingMargin": 12, "NetProfitMargin": 10,
+        "EBITDAMargin": 10, "ROA": 10, "ROE": 12,
+        "DebtToEquity": 10, "DebtToAssets": 10,
+        "CurrentRatio": 8, "FCFToRevenue": 9, "FCFYield": 9
+    },
+    "Distribution Services": {
+        "GrossMargin": 8, "OperatingMargin": 12, "NetProfitMargin": 8,
+        "EBITDAMargin": 10, "ROA": 12, "ROE": 12,
+        "DebtToEquity": 12, "DebtToAssets": 10,
+        "CurrentRatio": 8, "FCFToRevenue": 9, "FCFYield": 9
+    },
+    "Electronic Technology": {
+        "GrossMargin": 12, "OperatingMargin": 12, "NetProfitMargin": 12,
+        "EBITDAMargin": 10, "ROA": 10, "ROE": 12,
+        "DebtToEquity": 8, "DebtToAssets": 8,
+        "CurrentRatio": 8, "FCFToRevenue": 8, "FCFYield": 8
+    },
+    "Energy Minerals": {
+        "GrossMargin": 8, "OperatingMargin": 12, "NetProfitMargin": 8,
+        "EBITDAMargin": 12, "ROA": 10, "ROE": 10,
+        "DebtToEquity": 12, "DebtToAssets": 12,
+        "CurrentRatio": 8, "FCFToRevenue": 10, "FCFYield": 10
+    },
+    "Finance": {
+        "GrossMargin": 0, "OperatingMargin": 8, "NetProfitMargin": 12,
+        "EBITDAMargin": 0, "ROA": 12, "ROE": 16,
+        "DebtToEquity": 20, "DebtToAssets": 16,
+        "CurrentRatio": 0, "FCFToRevenue": 8, "FCFYield": 8
+    },
+    "Health Services": {
+        "GrossMargin": 10, "OperatingMargin": 12, "NetProfitMargin": 10,
+        "EBITDAMargin": 10, "ROA": 10, "ROE": 12,
+        "DebtToEquity": 10, "DebtToAssets": 10,
+        "CurrentRatio": 8, "FCFToRevenue": 9, "FCFYield": 9
+    },
+    "Health Technology": {
+        "GrossMargin": 12, "OperatingMargin": 12, "NetProfitMargin": 12,
+        "EBITDAMargin": 10, "ROA": 10, "ROE": 10,
+        "DebtToEquity": 8, "DebtToAssets": 8,
+        "CurrentRatio": 8, "FCFToRevenue": 8, "FCFYield": 10
+    },
+    "Industrial Services": {
+        "GrossMargin": 8, "OperatingMargin": 12, "NetProfitMargin": 8,
+        "EBITDAMargin": 10, "ROA": 12, "ROE": 12,
+        "DebtToEquity": 12, "DebtToAssets": 10,
+        "CurrentRatio": 8, "FCFToRevenue": 10, "FCFYield": 10
+    },
+    "Non-Energy Minerals": {
+        "GrossMargin": 8, "OperatingMargin": 12, "NetProfitMargin": 8,
+        "EBITDAMargin": 12, "ROA": 10, "ROE": 10,
+        "DebtToEquity": 12, "DebtToAssets": 12,
+        "CurrentRatio": 8, "FCFToRevenue": 10, "FCFYield": 10
+    },
+    "Process Industries": {
+        "GrossMargin": 10, "OperatingMargin": 12, "NetProfitMargin": 10,
+        "EBITDAMargin": 12, "ROA": 10, "ROE": 10,
+        "DebtToEquity": 12, "DebtToAssets": 10,
+        "CurrentRatio": 8, "FCFToRevenue": 8, "FCFYield": 8
+    },
+    "Producer Manufacturing": {
+        "GrossMargin": 10, "OperatingMargin": 12, "NetProfitMargin": 10,
+        "EBITDAMargin": 10, "ROA": 10, "ROE": 12,
+        "DebtToEquity": 12, "DebtToAssets": 10,
+        "CurrentRatio": 8, "FCFToRevenue": 8, "FCFYield": 8
+    },
+    "Retail Trade": {
+        "GrossMargin": 10, "OperatingMargin": 12, "NetProfitMargin": 10,
+        "EBITDAMargin": 10, "ROA": 12, "ROE": 12,
+        "DebtToEquity": 10, "DebtToAssets": 10,
+        "CurrentRatio": 8, "FCFToRevenue": 8, "FCFYield": 8
+    },
+    "Technology Services": {
+        "GrossMargin": 12, "OperatingMargin": 12, "NetProfitMargin": 12,
+        "EBITDAMargin": 10, "ROA": 10, "ROE": 10,
+        "DebtToEquity": 8, "DebtToAssets": 8,
+        "CurrentRatio": 8, "FCFToRevenue": 8, "FCFYield": 10
+    },
+    "Transportation": {
+        "GrossMargin": 8, "OperatingMargin": 12, "NetProfitMargin": 8,
+        "EBITDAMargin": 12, "ROA": 10, "ROE": 10,
+        "DebtToEquity": 12, "DebtToAssets": 12,
+        "CurrentRatio": 8, "FCFToRevenue": 10, "FCFYield": 10
+    },
+    "Utilities": {
+        "GrossMargin": 8, "OperatingMargin": 10, "NetProfitMargin": 8,
+        "EBITDAMargin": 12, "ROA": 10, "ROE": 10,
+        "DebtToEquity": 14, "DebtToAssets": 12,
+        "CurrentRatio": 8, "FCFToRevenue": 10, "FCFYield": 10
+    },
+    "Default": {
+        "GrossMargin": 8, "OperatingMargin": 12, "NetProfitMargin": 8,
+        "EBITDAMargin": 10, "ROA": 10, "ROE": 12,
+        "DebtToEquity": 12, "DebtToAssets": 10,
+        "CurrentRatio": 8, "FCFToRevenue": 10, "FCFYield": 10
+    }
+}
