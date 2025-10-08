a0fdee554ee546ba2953f53bb4db73b8411a8f06
laceto <l_aceto@hotmail.it>
2025-10-09 00:04:30 +0200

    add try to download

--- download_fin_data.py
+++ download_fin_data.py
@@ -1,3 +1,10 @@
+import pathlib
+
+data_dir = pathlib.Path("./financial_data")
+parquet_files = data_dir.glob("*.parquet")
+
+downloaded_tickers = [f.stem.replace("_merged_data", "").replace("_info", "") for f in parquet_files]
+
 import json
 import pandas as pd
 
@@ -9,13 +16,22 @@ with open('./financialtools/data/company_tickers.json') as f:
 tickers = pd.DataFrame.from_dict(data, orient='index').reset_index()
 tickers.drop(columns='index', inplace=True)
 tickers = tickers['ticker'].to_list()
-tickers = tickers[:5]
-from financialtools.processor import Downloader, RateLimiter  # Assume the updated Downloader class is in downloader.py
+print(len(tickers))
+tickers_filtered = [t for t in tickers if t not in downloaded_tickers]
+print(len(tickers_filtered))
 
-limiter = RateLimiter(per_minute=60, per_hour=360, per_day=8000)
+# tickers = tickers[:5]
+# from financialtools.processor import Downloader, RateLimiter  # Assume the updated Downloader class is in downloader.py
 
-for dl in Downloader.stream_download(tickers, limiter):
-    print(f"{dl.ticker} ready → {len(dl._balance_sheet)} rows")
+# limiter = RateLimiter(per_minute=60, per_hour=360, per_day=8000)
+
+# for dl in Downloader.stream_download(tickers, limiter):
+#     print(f"{dl.ticker} ready → {len(dl._balance_sheet)} rows")
+
+
+
+
+# print(downloaded_tickers)
 
 
 
