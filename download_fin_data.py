bcaea6afa421c77c404b3d735d21833c8d5814b0
laceto <l_aceto@hotmail.it>
2025-10-08 20:58:42 +0200

    add script to stream download

--- download_fin_data.py
+++ download_fin_data.py
@@ -10,26 +10,12 @@ tickers = pd.DataFrame.from_dict(data, orient='index').reset_index()
 tickers.drop(columns='index', inplace=True)
 tickers = tickers['ticker'].to_list()
 
-from financialtools.processor import Downloader  # Assume the updated Downloader class is in downloader.py
-import os
+from financialtools.processor import Downloader, RateLimiter  # Assume the updated Downloader class is in downloader.py
 
-# List of tickers, including an invalid ticker (DVLT)
-# tickers = tickers[:50]
+limiter = RateLimiter(per_minute=60, per_hour=360, per_day=8000)
 
-# Set custom cache location
-cache_path = "./cache"
-os.makedirs(cache_path, exist_ok=True)  # Create cache directory if it doesn't exist
-
-# Fetch data in batches of 4 with 1-second sleep, using custom cache
-downloaders = Downloader.from_ticker(tickers, batch_size=5, sleep_time=2.0, max_workers=5, 
-                                    verbose=False, cache_location=cache_path)
-
-financial_data = Downloader.combine_merged_data(downloaders)
-info_data = Downloader.combine_info_data(downloaders)
-
-from financialtools.utils import export_to_xlsx
-export_to_xlsx(df=financial_data, path='financial_data/sec_financial_data.xlsx', sheet_name='sheet1')
-export_to_xlsx(df=financial_data, path='financial_data/sec_info_data.xlsx', sheet_name='sheet1')
+for dl in Downloader.stream_download(tickers, limiter):
+    print(f"{dl.ticker} ready → {len(dl._balance_sheet)} rows")
 
 
 
